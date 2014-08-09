4d9a077d536ad19d26df0fa2d79bfbfa19011505
ongaeshi <ongaeshi0621@gmail.com>
2014-08-09 23:01:38 +0900

    Remove SecondViewController

--- apps/myApps/ofruby/src/SecondViewController.h
+++
@@ -1,4 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface SecondViewController : UIViewController
-@end
--- apps/myApps/ofruby/src/TopViewController.h
+++ apps/myApps/ofruby/src/TopViewController.h
@@ -1,8 +1,6 @@
 #import <UIKit/UIKit.h>
-#import "SecondViewController.h"
 
 @interface TopViewController : UITableViewController {
-    SecondViewController *secondController;
     NSArray *dataSource;
 }
 
