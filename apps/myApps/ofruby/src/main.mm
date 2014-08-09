4c3c09de509a7c7ca2d54298435cd1d4586329aa
ongaeshi <ongaeshi0621@gmail.com>
2014-08-09 17:16:12 +0900

    UIKit coexists with openFrameworks

--- apps/myApps/ofruby/src/main.mm
+++ apps/myApps/ofruby/src/main.mm
@@ -1,15 +1,8 @@
-//#include "ofMain.h"
-//#include "ofApp.h"
-#include <UIKit/UIKit.h>
-#import "SampleAppDelegate.h"
+#include "ofMain.h"
+#include "ofApp.h"
 
-int main(int argc, char *argv[]){
-    //	ofSetupOpenGL(1024,768,OF_FULLSCREEN);			// <-------- setup the GL context
-    //
-    //	ofRunApp(new ofApp());
-
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SampleAppDelegate class]));
-    }
-    
+int main(int argc, char *argv[]) {
+    ofAppiOSWindow *window = new ofAppiOSWindow();
+    ofSetupOpenGL(ofPtr<ofAppBaseWindow>(window), 1024,768, OF_FULLSCREEN);
+    window->startAppWithDelegate("SampleAppDelegate");
 }
