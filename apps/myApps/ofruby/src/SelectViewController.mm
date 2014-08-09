b1cf8d0f22a9ebb0c3ad38f3d2c37279bf7007f6
ongaeshi <ongaeshi0621@gmail.com>
2014-08-09 23:25:24 +0900

    Create a file if not exists

--- apps/myApps/ofruby/src/SelectViewController.mm
+++ apps/myApps/ofruby/src/SelectViewController.mm
@@ -2,6 +2,7 @@
 #include "ScriptController.h"
 #include "ScriptApp.h"
 #include "EditViewController.h"
+#include "FCFileManager.h"
 
 @interface SelectViewController ()
 
@@ -45,20 +46,22 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString* fullFileName = [dataSource objectAtIndex:indexPath.row];
-    NSString* fileName = [[fullFileName lastPathComponent] stringByDeletingPathExtension];
-    NSString* extension = [fullFileName pathExtension];
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:fileName ofType:extension];
-        
-    char* scriptPath = (char *)[filePath UTF8String];
+    NSString* tableCellName = [dataSource objectAtIndex:indexPath.row];
+    NSString* path = [FCFileManager pathForDocumentsDirectoryWithPath:tableCellName];
+
+    // Create a file if not exists
+    if (![FCFileManager existsItemAtPath:path]) {
+        [FCFileManager createFileAtPath:path withContent:@"def draw\n  rect 150, 0, 40, 480\nend\n"];
+    }
     
-//    ScriptController* viewController = [[[ScriptController alloc] initWithFrame:[[UIScreen mainScreen] bounds]
-//                                                                     scriptName:scriptPath] autorelease];
-    
-    EditViewController* viewController = [[[EditViewController alloc] init] autorelease];
+    // EditViewController* viewController = [[[EditViewController alloc] init] autorelease];
+
+    char* scriptPath = (char *)[path UTF8String];
+    ScriptController* viewController = [[[ScriptController alloc] initWithFrame:[[UIScreen mainScreen] bounds]
+                                                                    scriptName:scriptPath] autorelease];
 
     [self.navigationController pushViewController:viewController animated:YES];
-    self.navigationController.navigationBar.topItem.title = fullFileName;
+    self.navigationController.navigationBar.topItem.title = tableCellName;
 }
 
 @end
