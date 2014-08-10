52b30c42caacadd7416433f74ee07c2497e1f31c
ongaeshi <ongaeshi0621@gmail.com>
2014-08-10 19:05:04 +0900

    Refactor

--- apps/myApps/ofruby/src/EditViewController.h
+++ apps/myApps/ofruby/src/EditViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface EditViewController : UIViewController {
+@private
     NSString *mFileName;
 }
 
--- apps/myApps/ofruby/src/EditViewController.mm
+++ apps/myApps/ofruby/src/EditViewController.mm
@@ -8,11 +8,7 @@
 
 #import "EditViewController.h"
 
-#include "FCFileManager.h"
-
-@interface EditViewController ()
-
-@end
+#import "FCFileManager.h"
 
 @implementation EditViewController
 
@@ -45,16 +41,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
