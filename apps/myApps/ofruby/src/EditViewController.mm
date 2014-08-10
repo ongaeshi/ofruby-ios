f2f0003225a1acb0abea896f8fce2d0a2f22cbc7
ongaeshi <ongaeshi0621@gmail.com>
2014-08-10 18:23:34 +0900

    Remove EditViewController#initWithNibName

--- apps/myApps/ofruby/src/EditViewController.mm
+++ apps/myApps/ofruby/src/EditViewController.mm
@@ -23,15 +23,6 @@
     return self;
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
