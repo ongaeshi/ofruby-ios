9fb5aaceeab84c7a1ef90e1ea5e952c23945900e
ongaeshi <ongaeshi0621@gmail.com>
2014-08-25 01:07:29 +0900

    Implement BindTouchPoint::ToMrb and the TouchPoint methods

--- apps/myApps/ofruby/src/rubykokuban/BindTouchPoint.cpp
+++ apps/myApps/ofruby/src/rubykokuban/BindTouchPoint.cpp
@@ -1,36 +1,53 @@
 #include "BindTouchPoint.hpp"
 
 #include "mruby/value.h"
+#include "mruby/class.h"
+#include "mruby/data.h"
+#include "mruby/value.h"
+#include "rubykokuban/TouchPoint.hpp"
 
 namespace rubykokuban {
 
 namespace {
+TouchPoint& obj(mrb_value self)
+{
+    return *((TouchPoint*)DATA_PTR(self));
+}
+
+// TouchPoint instance has C++
+struct mrb_data_type data_type = { "rubykokuban_touch_point", NULL };
+
 mrb_value x(mrb_state *mrb, mrb_value self)
 {
-    // return self;
-    return mrb_nil_value();
+    return mrb_fixnum_value(obj(self).x());
 }
 
 mrb_value y(mrb_state *mrb, mrb_value self)
 {
-    // return self;
-    return mrb_nil_value();
+    return mrb_fixnum_value(obj(self).y());
 }
 
 mrb_value valid_p(mrb_state *mrb, mrb_value self)
 {
-    // return self;
-    return mrb_nil_value();
+    return mrb_bool_value(obj(self).isValid());
 }
 
-mrb_value inspect(mrb_state *mrb, mrb_value self)
+}
+
+//----------------------------------------------------------
+mrb_value BindTouchPoint::ToMrb(mrb_state* mrb, TouchPoint* aPtr)
 {
-    // return self;
-    return mrb_nil_value();
+    return ToMrb(mrb, mrb_class_get(mrb, "TouchPoint"), aPtr);
 }
 
+//----------------------------------------------------------
+mrb_value BindTouchPoint::ToMrb(mrb_state* mrb, struct RClass* aClass, TouchPoint* aPtr)
+{
+    struct RData *data = mrb_data_object_alloc(mrb, aClass, aPtr, &data_type);
+    return mrb_obj_value(data);
 }
 
+//----------------------------------------------------------
 void BindTouchPoint::Bind(mrb_state* mrb)
 {
     struct RClass *cc = mrb_define_class(mrb, "TouchPoint", mrb->object_class);
@@ -38,7 +55,6 @@ void BindTouchPoint::Bind(mrb_state* mrb)
     mrb_define_method(mrb, cc,       "x"                 , x                   , MRB_ARGS_NONE());
     mrb_define_method(mrb, cc,       "y"                 , y                   , MRB_ARGS_NONE());
     mrb_define_method(mrb, cc,       "valid?"            , valid_p             , MRB_ARGS_NONE());
-    mrb_define_method(mrb, cc,       "inspect"           , inspect             , MRB_ARGS_NONE());
 }
 
 }
--- apps/myApps/ofruby/src/rubykokuban/BindTouchPoint.hpp
+++ apps/myApps/ofruby/src/rubykokuban/BindTouchPoint.hpp
@@ -2,11 +2,17 @@
 
 #include "mruby.h"
 
+namespace rubykokuban {
+    class TouchPoint;
+}
+
 //----------------------------------------------------------
 namespace rubykokuban {
 
 class BindTouchPoint {
 public:
+    static mrb_value ToMrb(mrb_state* mrb, TouchPoint* aPtr);
+    static mrb_value ToMrb(mrb_state* mrb, struct RClass* aClass, TouchPoint* aPtr);
     static void Bind(mrb_state* mrb);
 };
 
