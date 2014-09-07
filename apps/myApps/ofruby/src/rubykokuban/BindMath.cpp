b9046f970cd12a5bf6efc8ad459301f34cc3b530
ongaeshi <ongaeshi0621@gmail.com>
2014-09-07 12:00:30 +0900

    Bind the funcitons of ofMath
    dist
    dist_squared
    clamp
    normalize
    rad_to_deg
    deg_to_rad
    lerp

--- apps/myApps/ofruby/src/rubykokuban/BindMath.cpp
+++ apps/myApps/ofruby/src/rubykokuban/BindMath.cpp
@@ -48,6 +48,55 @@ mrb_value noise(mrb_state *mrb, mrb_value self)
     }
 }
 
+mrb_value dist(mrb_state *mrb, mrb_value self)
+{
+    mrb_float x1, y1, x2, y2;
+    mrb_get_args(mrb, "ffff", &x1, &y1, &x2, &y2);
+    return mrb_float_value(mrb, ofDist(x1, y1, x2, y2));
+}
+
+mrb_value dist_squared(mrb_state *mrb, mrb_value self)
+{
+    mrb_float x1, y1, x2, y2;
+    mrb_get_args(mrb, "ffff", &x1, &y1, &x2, &y2);
+    return mrb_float_value(mrb, ofDistSquared(x1, y1, x2, y2));
+}
+
+mrb_value clamp(mrb_state *mrb, mrb_value self)
+{
+    mrb_float value, min, max;
+    mrb_get_args(mrb, "fff", &value, min, max);
+    return mrb_float_value(mrb, ofClamp(value, min, max));
+}
+
+mrb_value normalize(mrb_state *mrb, mrb_value self)
+{
+    mrb_float value, min, max;
+    mrb_get_args(mrb, "fff", &value, min, max);
+    return mrb_float_value(mrb, ofNoise(value, min, max));
+}
+
+mrb_value rad_to_deg(mrb_state *mrb, mrb_value self)
+{
+    mrb_float radians;
+    mrb_get_args(mrb, "f", &radians);
+    return mrb_float_value(mrb, ofRadToDeg(radians));
+}
+
+mrb_value deg_to_rad(mrb_state *mrb, mrb_value self)
+{
+    mrb_float degrees;
+    mrb_get_args(mrb, "f", &degrees);
+    return mrb_float_value(mrb, ofDegToRad(degrees));
+}
+
+mrb_value lerp(mrb_state *mrb, mrb_value self)
+{
+    mrb_float start, stop, amt;
+    mrb_get_args(mrb, "fff", &start, stop, amt);
+    return mrb_float_value(mrb, ofLerp(start, stop, amt));
+}
+
 }
 
 //--------------------------------------------------------------------------------
@@ -56,6 +105,13 @@ void BindMath(mrb_state* mrb)
     mrb_define_module_function(mrb, mrb->kernel_module, "srand"             , srand               , MRB_ARGS_REQ(0));
     mrb_define_module_function(mrb, mrb->kernel_module, "rand"              , rand                , MRB_ARGS_OPT(1));
     mrb_define_module_function(mrb, mrb->kernel_module, "noise"             , noise               , MRB_ARGS_ARG(1, 2));
+    mrb_define_module_function(mrb, mrb->kernel_module, "dist"              , dist                , MRB_ARGS_REQ(4));
+    mrb_define_module_function(mrb, mrb->kernel_module, "dist_squared"      , dist_squared        , MRB_ARGS_REQ(4));
+    mrb_define_module_function(mrb, mrb->kernel_module, "clamp"             , clamp               , MRB_ARGS_REQ(3));
+    mrb_define_module_function(mrb, mrb->kernel_module, "normalize"         , normalize           , MRB_ARGS_REQ(3));
+    mrb_define_module_function(mrb, mrb->kernel_module, "rad_to_deg"        , rad_to_deg          , MRB_ARGS_REQ(1));
+    mrb_define_module_function(mrb, mrb->kernel_module, "deg_to_rad"        , deg_to_rad          , MRB_ARGS_REQ(1));
+    mrb_define_module_function(mrb, mrb->kernel_module, "lerp"              , lerp                , MRB_ARGS_REQ(3));
 
 }
 
