300d119fd23e2cc9ea528325dca29b494d39abed
ongaeshi <ongaeshi0621@gmail.com>
2014-09-07 11:21:11 +0900

    Bind noise(x), noise(x, y), noise(x, y, z)

--- apps/myApps/ofruby/src/rubykokuban/BindMath.cpp
+++ apps/myApps/ofruby/src/rubykokuban/BindMath.cpp
@@ -7,37 +7,57 @@
 namespace rubykokuban {
 
 namespace {
-    static mrb_value srand(mrb_state *mrb, mrb_value self)
-    {
-        mrb_int seed;
+mrb_value srand(mrb_state *mrb, mrb_value self)
+{
+    mrb_int seed;
 
-        if (mrb_get_args(mrb, "|i", &seed) == 1) {
-            ofSeedRandom(seed);
-        } else {
-            ofSeedRandom();
-        }
-
-        return mrb_nil_value();
+    if (mrb_get_args(mrb, "|i", &seed) == 1) {
+        ofSeedRandom(seed);
+    } else {
+        ofSeedRandom();
     }
 
-    static mrb_value rand(mrb_state *mrb, mrb_value self)
-    {
-        mrb_float max;
+    return mrb_nil_value();
+}
+
+mrb_value rand(mrb_state *mrb, mrb_value self)
+{
+    mrb_float max;
     
-        if (mrb_get_args(mrb, "|f", &max) == 1) {
-            return mrb_float_value(mrb, ofRandom(max));
-        } else {
-            return mrb_float_value(mrb, ofRandomuf());
-        }
+    if (mrb_get_args(mrb, "|f", &max) == 1) {
+        return mrb_float_value(mrb, ofRandom(max));
+    } else {
+        return mrb_float_value(mrb, ofRandomuf());
     }
+}
+
+mrb_value noise(mrb_state *mrb, mrb_value self)
+{
+    mrb_float x, y, z;
+    int num = mrb_get_args(mrb, "f|ff", &x, &y, &z);
+
+    switch (num) {
+        case 1:
+            return mrb_float_value(mrb, ofNoise(x));
+        case 2:
+            return mrb_float_value(mrb, ofNoise(x, y));
+        case 3:
+            return mrb_float_value(mrb, ofNoise(x, y, z));
+        default:
+            return mrb_nil_value();
+    }
+}
+
 }
 
 //--------------------------------------------------------------------------------
 void BindMath(mrb_state* mrb)
 {
     // Use mruby-random now. (Thinking in...)
-    mrb_define_method(mrb, mrb->kernel_module, "srand", srand, MRB_ARGS_REQ(0));
-    mrb_define_method(mrb, mrb->kernel_module, "rand", rand, MRB_ARGS_OPT(1));
+    mrb_define_method(mrb, mrb->kernel_module, "srand"             , srand               , MRB_ARGS_REQ(0));
+    mrb_define_method(mrb, mrb->kernel_module, "rand"              , rand                , MRB_ARGS_OPT(1));
+    mrb_define_method(mrb, mrb->kernel_module, "noise"             , noise               , MRB_ARGS_ARG(1, 2));
+
 }
 
 }
