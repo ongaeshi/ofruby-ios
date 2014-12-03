8ee9073b9a2bb75fc846503851597c0c5755bea0
ongaeshi <ongaeshi0621@gmail.com>
2014-12-03 22:53:33 +0900

    Support Xcode 6
    ref: https://github.com/openframeworks/openFrameworks/issues/3178

--- apps/myApps/ofruby/src/main.mm
+++ apps/myApps/ofruby/src/main.mm
@@ -1,6 +1,24 @@
 #include "ofMain.h"
 #include "ofAppiOSWindow.h"
 
+extern "C" {
+    size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d )
+    {
+        return fwrite(a, b, c, d);
+    }
+    char* strerror$UNIX2003( int errnum )
+    {
+        return strerror(errnum);
+    }
+    time_t mktime$UNIX2003(struct tm * a)
+    {
+        return mktime(a);
+    }
+    double strtod$UNIX2003(const char * a, char ** b) {
+        return strtod(a, b);
+    }
+}
+
 int main(int argc, char *argv[]) {
     ofAppiOSWindow *window = new ofAppiOSWindow();
     ofSetupOpenGL(ofPtr<ofAppBaseWindow>(window), 1024,768, OF_FULLSCREEN);
