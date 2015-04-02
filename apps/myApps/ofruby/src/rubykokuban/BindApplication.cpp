55b64c9ffd83d94d831a865e6c2094e66c4a636a
ongaeshi <ongaeshi0621@gmail.com>
2015-04-03 01:30:40 +0900

    Rewrite BindApplication.cpp

--- apps/myApps/ofruby/src/rubykokuban/BindApplication.cpp
+++ apps/myApps/ofruby/src/rubykokuban/BindApplication.cpp
@@ -1,43 +1,105 @@
 #include "rubykokuban/Bind.hpp"
 
 #include "mruby.h"
-// #include "mrubybind.h"
+#include "mruby/value.h"
 #include "ofAppRunner.h"
 
 namespace rubykokuban {
 
 namespace {
-    float frame_rate() { return ofGetFrameRate(); }
-    void set_window_pos(int x, int y) { ofSetWindowPosition(x, y); }
-    void set_window_size(int width, int height) { ofSetWindowShape(width, height); }
-    int window_pos_x() { return ofGetWindowPositionX(); }
-    int window_pos_y() { return ofGetWindowPositionY(); }
-    // int window_width() { return ofGetWidth(); }
-    // int window_height() { return ofGetHeight(); }
-    int window_width() { return ofGetWindowWidth(); }
-    int window_height() { return ofGetWindowHeight(); }
-    int screen_width() { return ofGetScreenWidth(); }
-    int screen_height() { return ofGetScreenHeight(); }
-    int width() { return ofGetWidth(); }
-    int height() { return ofGetHeight(); }
+mrb_value frame_rate(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value set_window_pos(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value set_window_size(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value window_pos_x(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value window_pos_y(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value window_width(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value window_height(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value screen_width(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value screen_height(mrb_state *mrb, mrb_value self)
+{
+    // return self;
+    return mrb_nil_value();
+}
+
+mrb_value width(mrb_state *mrb, mrb_value self)
+{
+    return mrb_fixnum_value(ofGetWidth());
+}
+
+mrb_value height(mrb_state *mrb, mrb_value self)
+{
+    return mrb_fixnum_value(ofGetHeight());
+}
+
+    // float frame_rate() { return ofGetFrameRate(); }
+    // void set_window_pos(int x, int y) { ofSetWindowPosition(x, y); }
+    // void set_window_size(int width, int height) { ofSetWindowShape(width, height); }
+    // int window_pos_x() { return ofGetWindowPositionX(); }
+    // int window_pos_y() { return ofGetWindowPositionY(); }
+    // // int window_width() { return ofGetWidth(); }
+    // // int window_height() { return ofGetHeight(); }
+    // int window_width() { return ofGetWindowWidth(); }
+    // int window_height() { return ofGetWindowHeight(); }
+    // int screen_width() { return ofGetScreenWidth(); }
+    // int screen_height() { return ofGetScreenHeight(); }
+    // int width() { return ofGetWidth(); }
+    // int height() { return ofGetHeight(); }
 }
 
 //----------------------------------------------------------
 void BindApplication(mrb_state* mrb)
 {
-    // mrubybind::MrubyBind b(mrb);
-
-    // b.bind("frame_rate", frame_rate);
-    // b.bind("set_window_pos", set_window_pos);
-    // b.bind("set_window_size", set_window_size);
-    // b.bind("window_pos_x", window_pos_x);
-    // b.bind("window_pos_y", window_pos_y);
-    // b.bind("window_width", window_width);
-    // b.bind("window_height", window_height);
-    // b.bind("screen_width", screen_width);
-    // b.bind("screen_height", screen_height);
-    // b.bind("width", width);
-    // b.bind("height", height);
+    mrb_define_module_function(mrb, mrb->kernel_module, "frame_rate"        , frame_rate          , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "set_window_pos"    , set_window_pos      , MRB_ARGS_REQ(2));
+    mrb_define_module_function(mrb, mrb->kernel_module, "set_window_size"   , set_window_size     , MRB_ARGS_REQ(2));
+    mrb_define_module_function(mrb, mrb->kernel_module, "window_pos_x"      , window_pos_x        , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "window_pos_y"      , window_pos_y        , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "window_width"      , window_width        , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "window_height"     , window_height       , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "screen_width"      , screen_width        , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "screen_height"     , screen_height       , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "width"             , width               , MRB_ARGS_NONE());
+    mrb_define_module_function(mrb, mrb->kernel_module, "height"            , height              , MRB_ARGS_NONE());
 }
 
 }
