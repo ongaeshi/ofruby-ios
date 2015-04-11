8cd507907d26ed51f688ec4218f8da0357031fae
ongaeshi <ongaeshi0621@gmail.com>
2015-04-12 02:08:24 +0900

    Display name if you touch the icon

--- apps/myApps/ofruby/resources/sample/emoji.rb
+++ apps/myApps/ofruby/resources/sample/emoji.rb
@@ -1,5 +1,5 @@
 X = 4
-MOVABLE_RANGE = 200
+MOVABLE_RANGE = 50
 PAGE = 13
 
 def setup
@@ -11,13 +11,17 @@ def setup
   end
 
   @screen_y = 0
+  @text_emoji = nil
 end
 
 def update
   t = Input.touch(0)
   
   if t.valid? && t.press?
-    if t.y > (height - MOVABLE_RANGE)
+    emoji = @emojis.detect { |e| e.update(t, -@screen_y) }
+    if emoji
+      @text_emoji = emoji
+    elsif t.y > (height - MOVABLE_RANGE)
       if @screen_y > height * -(PAGE - 1)
         @screen_y -= height
       end
@@ -30,28 +34,39 @@ def update
 end
 
 def draw
-  push_matrix do |x|
+  push_matrix do
     translate 0, @screen_y
     @emojis.each { |e| e.draw }
+    @text_emoji.draw_text(10, -@screen_y + height - 10) if @text_emoji
   end
 end
 
 class Emoji
+  attr_reader :name
+  
   def initialize(i, x, y)
     @x = x
     @y = y
     @emoji, @name = Image.emoji_index(i)
   end
 
+  def update(t, offset_y)
+    @x <= t.x && t.x <= @x + 32 &&
+      @y <= t.y + offset_y && t.y + offset_y <= @y + 32
+  end
+
   def draw
     set_color_hex 0xffffff
-    @emoji.draw(@x, @y)
-
     push_matrix do
-      translate @x, @y + 50
-      scale(1, 1)
+      translate 10, 0
+      @emoji.draw(@x, @y)
+    end
+  end
+
+  def draw_text(x, y)
+    push_matrix do
       set_color_hex 0x000000
-      # text "#{@name.to_s}", 0, 0
+      text "#{@name.to_s}", x, y
     end
   end
 end
