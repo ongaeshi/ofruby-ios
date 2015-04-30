b4277eb110883e9cb07e0217567e7d0baaf042bd
ongaeshi <ongaeshi0621@gmail.com>
2015-04-30 11:51:19 +0900

    Data reduction

--- apps/myApps/ofruby/src/SyntaxHighlighter.m
+++ apps/myApps/ofruby/src/SyntaxHighlighter.m
@@ -6,16 +6,24 @@
 {
     NSMutableAttributedString *dst = [[NSMutableAttributedString alloc] initWithString:src];
     NSRange srcRange = NSMakeRange(0,[src length]);
-    
-    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"#[^\r\n]*[\r\n]"
-                                                                           options:NSRegularExpressionDotMatchesLineSeparators
-                                                                             error:nil];
-    NSArray* matches = [regex matchesInString:src options:0 range:srcRange];
 
-    for (NSTextCheckingResult* match in matches) {
-        [dst addAttribute:NSForegroundColorAttributeName
-                    value:[UIColor colorWithRed:0.00 green:0.50 blue:0.00 alpha:1.0]
-                    range:[match rangeAtIndex:0]];
+    NSArray* syntaxArray = [[NSArray alloc] initWithObjects:
+        //                                pattern,            color
+        [[NSArray alloc] initWithObjects: @"#[^\r\n]*[\r\n]", [UIColor colorWithRed:0.00 green:0.50 blue:0.00 alpha:1.0], nil],
+        nil
+        ];
+
+    for (NSArray* highlightRule in syntaxArray) {
+        NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:highlightRule[0]
+                                                                               options:NSRegularExpressionDotMatchesLineSeparators
+                                                                                 error:nil];
+        NSArray* matches = [regex matchesInString:src options:0 range:srcRange];
+
+        for (NSTextCheckingResult* match in matches) {
+            [dst addAttribute:NSForegroundColorAttributeName
+                        value:highlightRule[1]
+                        range:[match rangeAtIndex:0]];
+        }
     }
 
     return dst;
