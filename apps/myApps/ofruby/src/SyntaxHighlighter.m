96083c4936d98af52b3ddf29f1506fcea7315666
ongaeshi <ongaeshi0621@gmail.com>
2015-04-30 11:40:20 +0900

    Highlight comment

--- apps/myApps/ofruby/src/SyntaxHighlighter.m
+++ apps/myApps/ofruby/src/SyntaxHighlighter.m
@@ -2,13 +2,21 @@
 
 @implementation SyntaxHighlighter
 
-+ (NSAttributedString *)convertAttributedText:(NSString *)code
++ (NSAttributedString *)convertAttributedText:(NSString *)src
 {
-    NSMutableAttributedString *dst = [[NSMutableAttributedString alloc] initWithString:code];
+    NSMutableAttributedString *dst = [[NSMutableAttributedString alloc] initWithString:src];
+    NSRange srcRange = NSMakeRange(0,[src length]);
     
-    [dst addAttribute:NSForegroundColorAttributeName
-                value:[UIColor colorWithRed:0.00 green:0.50 blue:0.00 alpha:1.0]
-                range:NSMakeRange(0, 10)];
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:@"#[^\r\n]*[\r\n]"
+                                                                           options:NSRegularExpressionDotMatchesLineSeparators
+                                                                             error:nil];
+    NSArray* matches = [regex matchesInString:src options:0 range:srcRange];
+
+    for (NSTextCheckingResult* match in matches) {
+        [dst addAttribute:NSForegroundColorAttributeName
+                    value:[UIColor colorWithRed:0.00 green:0.50 blue:0.00 alpha:1.0]
+                    range:[match rangeAtIndex:0]];
+    }
 
     return dst;
 }
